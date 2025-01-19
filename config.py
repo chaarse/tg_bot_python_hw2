981e15d2a458cfd5d75a2a8258a3b91638c0776d
Arina Chaplygina <arienachaplygina@yandex.ru>
2025-01-19 17:12:03 +0300

    add config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ CALORIES_API_KEY = os.getenv('CALORIES_API_KEY')
 if not API_TOKEN:
     raise ValueError('Переменная окружения BOT_TOKEN не установлена!')
 
-if not WEATHER_API_KEY:
-    raise ValueError('Переменная окружения WEATHER_API_KEY не установлена!')
-
-if not WEATHER_API_KEY:
-    raise ValueError('Переменная окружения CALORIES_API_KEY не установлена!')
+# if not WEATHER_API_KEY:
+#     raise ValueError('Переменная окружения WEATHER_API_KEY не установлена!')
+#
+# if not WEATHER_API_KEY:
+#     raise ValueError('Переменная окружения CALORIES_API_KEY не установлена!')
