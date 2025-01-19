cd8ad7a279f064adabd357e939ef972b701a8238
Arina Chaplygina <arienachaplygina@yandex.ru>
2025-01-19 18:49:05 +0300

    add bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import router
+import asyncio
 from config import API_TOKEN
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
-
 
 async def main():
     print('Бот запущен!')
@@ -14,5 +13,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
