b10537c614d3dfac96578b80f5915f2606e60e5f
Arina Chaplygina <arienachaplygina@yandex.ru>
2025-01-19 20:03:41 +0300

    add bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,15 @@ from handlers import router
 import asyncio
 from config import API_TOKEN
 
-# Создание бота и диспетчера
+# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 
-# Функция для запуска бота
+# Главная функция запуска
 async def main():
-    print('Бот запущен!')
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
